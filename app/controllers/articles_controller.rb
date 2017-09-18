b2745aa2a245810be7977a1e7088bc913ac1a65a
doc2git <doc2git@aliyun.com>
2017-09-18 19:04:02 +0800

    ruby-blog-follow-rails-tutor complete, but without user security authencation, on the right side of test new codes in w3m.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,20 +1,15 @@
 class ArticlesController < ApplicationController
-  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
   def index
     @articles = Article.all
   end
-  def show
-    @article = Article.find(params[:id])
-  end
+
+
   def new
     @article = Article.new
   end
-  def edit
-    @article = Article.find(params[:id])
-  end
+
   def create
     # render plain: params[:article].inspect
-    # @article = Article.new(params.require(:article).permit(:title, :text))
     @article = Article.new(article_params)
     if @article.save
       redirect_to @article
@@ -22,6 +17,24 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def update
     @article = Article.find(params[:id])
     if @article.update(article_params)
@@ -30,14 +43,10 @@ class ArticlesController < ApplicationController
       render 'edit'
     end
   end
-  def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-    redirect_to articles_path
-  end
+
 
   private
-  def article_params
-    params.require(:article).permit(:title, :text)
-  end
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,4 @@
 class WelcomeController < ApplicationController
-  def index
+  def dindex
   end
 end
